dd8a02f95ab34994c5ac061aba27c6f2c255c264
Frankley Francalino da Rocha <rocha112358@gmail.com>
2024-05-31 07:22:08 -0300

    dados iniciais de status

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into status(id,nome, titulo) values (RANDOM_UUID(),'A_FAZER', 'A Fazer');
+insert into status(id,nome, titulo) values (RANDOM_UUID(),'FAZENDO', 'Fazendo');
+insert into status(id,nome, titulo) values (RANDOM_UUID(),'FEITO', 'Feito');
+insert into status(id,nome, titulo) values (RANDOM_UUID(),'ARQUIVADO', 'Arquivado');
